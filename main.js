7bc2e1120e05915cd697697cd2136da9378706a8
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-26 16:29:19 +0100

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,11 @@
+// $('.hamburger-menu').click('display': 'block')
+//
+// $('.close').click('display': 'none')
+
+$('.fas').click(function(){
+  $('.hamburger-menu').show()
+})
+
+$('.close').click(function(){
+  $('.hamburger-menu').hide()
+})
